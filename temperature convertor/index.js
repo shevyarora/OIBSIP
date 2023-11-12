947d55af51fc2260c3fca6c924edbc9fbf159632
shevyarora <shevykumar26@gmail.com>
2023-11-13 02:56:25 +0530

    Add files via upload
    Task -3

---
+++ temperature convertor/index.js
@@ -0,0 +1,48 @@
+function Convert(){
+    let result;
+    let degrees;
+    let val1;
+    let val2;
+   degrees=document.getElementById('degrees').value;
+   val1=document.getElementById('type').value;
+   val2=document.getElementById('type2').value;
+   
+   if(val1=="Fahrenheit" && val2== "Fahrenheit"){
+     result=degrees;
+     document.getElementById('result').value=result+" °F"
+   }
+   else if(val1=="Celsius" && val2=="Celsius"){
+    result=degrees;
+    document.getElementById('result').value=result+" °C"
+   }
+   else if(val1=="Kelvin" && val2=="Kelvin"){
+    result=degrees;
+    document.getElementById('result').value=result+" °K"
+   }
+   else if(val1=="Fahrenheit" && val2=="Celsius"){
+    result=(degrees - 32)*5/9;
+    document.getElementById('result').value=result+" °C"
+   }
+   else if(val1=="Fahrenheit" && val2=="Kelvin"){
+    result=((degrees - 32)*5/9)+273.16;
+    document.getElementById('result').value=result+" °K"
+   }
+   else if(val1=="Celsius" && val2=="Fahrenheit"){
+    result=9/5*(degrees)+32;
+    document.getElementById('result').value=result+" °F"
+   }
+   else if(val1=="Celsius" && val2=="Kelvin"){
+    result=(degrees+273.16);
+    document.getElementById('result').value=result+" °K"
+   }
+   else if(val1=="Kelvin" && val2=="Fahrenheit"){
+    result=(degrees - 273.16)*5/9+32;
+    document.getElementById('result').value=result+" °F"
+   }
+   else{
+    result=(degrees-273.16);
+    document.getElementById('result').value=result+" °C"
+   }
+   
+
+}
